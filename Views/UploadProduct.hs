5b0a0c17a72ec49b49c90e9d6bf64543324f750b
mlund <mlund@itba.edu.ar>
2021-02-08 03:26:25 -0300

    Add product upload indentation fix

--- Views/UploadProduct.hs
+++ Views/UploadProduct.hs
@@ -20,6 +20,75 @@ import           Text.Blaze.Html ((!), Html)
 import qualified Text.Blaze.Html4.Strict as H
 import qualified Text.Blaze.Html4.Strict.Attributes as A
 
+viewUploadProduct  :: AcidState Store -> ServerPart Response
+viewUploadProduct acid = do
+ pid   <- ProductId <$> lookRead "id"
+ mProd <- query' acid (ProductById pid)
+ case mProd of
+  Nothing ->
+   notFound $ template "Not found" [] $
+                do "Could not find a product with ID "
+                   H.toHtml (unProductId pid)
+  (Just p@(Product{..})) -> msum
+    [ do method GET
+         ok $ template "Upload Product" [] $ do
+          H.div ! A.class_ "container" $ do
+            H.h1 ! A.class_ "title" $ "Upload Product"
+            H.form  ! A.class_ "upload-product-form"
+                    ! A.enctype "multipart/form-data"
+                    ! A.method "POST"
+                    ! A.action (H.toValue $ "/edit?id=" ++ (show $ unProductId pid)) $ do
+                H.div ! A.class_ "form-item" $ do
+                    H.label "Product name" ! A.for "name"
+                    H.input ! A.type_ "text"
+                            ! A.name "name"
+                            ! A.id "name"
+                            ! A.size "80"
+                            ! A.value (H.toValue name)
+                H.div ! A.class_ "form-item" $ do
+                    H.label "Brand" ! A.for "brand"
+                    H.input ! A.type_ "text"
+                            ! A.name "brand"
+                            ! A.id "brand"
+                            ! A.size "40"
+                            ! A.value (H.toValue brand)
+                H.div ! A.class_ "form-item" $ do
+                    H.label "Category" ! A.for "category"
+                    --H.select ! A.id "category" $ [("1", "Opcion")]
+                H.div ! A.class_ "form-item" $ do
+                    H.label "Price" ! A.for "price"
+                    H.input ! A.type_ "number"
+                            ! A.name "price"
+                            ! A.id "price"
+                            ! A.size "20"
+                H.div ! A.class_ "form-item" $ do
+                    H.label "Description" ! A.for "description"
+                    H.textarea ! A.cols "80"
+                                ! A.rows "5"
+                                ! A.name "description" $ H.toHtml description
+                H.div ! A.class_ "upload-product-btn-container" $ do
+                    H.button ! A.name "upload" ! A.class_ "action-button"
+                            ! A.value "upload" $ "Upload"
+    , do method POST
+         productName    <- lookText' "name"
+         productBrand   <- lookText' "brand"
+         productDesc    <- lookText' "description"
+         now            <- liftIO $ getCurrentTime
+         productPrice   <- lookText' "price"
+
+         let updatedProduct =
+                 p { name = productName
+                   , brand = productBrand
+                   , description = productDesc
+                   , date = now
+                   , price = 1 --productPrice
+                   }
+         update' acid (UpdateProduct updatedProduct)
+         seeOther ("/product?id=" ++ (show $ unProductId pid)) (toResponse ())
+                  ]
+
+      where lookText' = fmap toStrict . lookText
+
 createProduct :: AcidState Store -> ServerPart Response
 createProduct acid = do
   method POST
@@ -27,74 +96,3 @@ createProduct acid = do
   prod <- update' acid (NewProduct now)
   let url = "/edit?id=" ++ show (unProductId $ productId prod)
   seeOther url (toResponse ())
-
-viewUploadProduct  :: AcidState Store -> ServerPart Response
-viewUploadProduct acid = do
-    pid   <- ProductId <$> lookRead "id"
-    mProd <- query' acid (ProductById pid)
-    case mProd of
-        Nothing ->
-            notFound $ template "Not Found" [] $
-                        do "Could not find a product with ID "
-                            H.toHtml (unProductId pid)
-        (Just p@(Product{..})) -> msum [ 
-                do method GET
-                    ok $ template "Upload Product" [] $ do
-                        H.div ! A.class_ "container" $ do
-                            H.h1 ! A.class_ "title" $ "Upload Product"
-                            H.form  ! A.class_ "upload-product-form"
-                                    ! A.enctype "multipart/form-data"
-                                    ! A.method "POST"
-                                    ! A.action (H.toValue $ "/edit?id=" ++ (show $ unProductId pid)) $ do
-                                H.div ! A.class_ "form-item" $ do
-                                    H.label "Product name" ! A.for "name"
-                                    H.input ! A.type_ "text"
-                                            ! A.name "name"
-                                            ! A.id "name"
-                                            ! A.size "80"
-                                            ! A.value "Laptop"
-                                H.div ! A.class_ "form-item" $ do
-                                    H.label "Brand" ! A.for "brand"
-                                    H.input ! A.type_ "text"
-                                            ! A.name "brand"
-                                            ! A.id "brand"
-                                            ! A.size "40"
-                                            ! A.value "Amazon Basics"
-                                H.div ! A.class_ "form-item" $ do
-                                    H.label "Category" ! A.for "category"
-                                    --H.select ! A.id "category" $ [("1", "Opcion")]
-                                H.div ! A.class_ "form-item" $ do
-                                    H.label "Price" ! A.for "price"
-                                    H.input ! A.type_ "number"
-                                            ! A.name "price"
-                                            ! A.id "price"
-                                            ! A.size "20"
-                                H.div ! A.class_ "form-item" $ do
-                                    H.label "Description" ! A.for "description"
-                                    H.textarea ! A.cols "80"
-                                                ! A.rows "5"
-                                                ! A.name "body" $ "Una breve descripcion"
-                                H.div ! A.class_ "upload-product-btn-container" $ do
-                                    H.button ! A.name "upload" ! A.class_ "action-button"
-                                            ! A.value "upload" $ "Upload"
-            , do method POST
-                        productName     <- lookText' "name"
-                        productBrand    <- lookText' "brand"
-                        productDesc     <- lookText' "description"
-                        now             <- liftIO $ getCurrentTime
-                        productPrice    <- lookText' "price"
-
-                        let updatedProduct =
-                            p { name = productName
-                            , brand = productBrand
-                            , description = productDesc
-                            , date = now
-                            , price = productPrice
-                            , category = 1
-                            }
-
-                        update' acid (UpdateProduct updatedProduct)
-                        
-                        seeOther ("/product?id=" ++ (show $ unProductId pid)) (toResponse ())
-            ]
-            where lookText' = fmap toStrict . lookText
